be990e05bcb810b7ba05a8d4ef4dbe8a4c467288
Jhampier Tucno <yantucnoperez@gmail.com>
2023-04-21 01:16:34 -0500

    Se agrego animaciones

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -7,11 +7,40 @@ import { Component, HostListener } from '@angular/core';
 })
 export class HeaderComponent {
   public click: boolean = false;
+  public text: string = 'Open the door for a spacious living';
 
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
     if (event.target.innerWidth < 768) {
       this.click = false;
+    }
+  }
+
+  ngAfterViewInit() {
+    this.typewriterEffect();
+  }
+
+  typewriterEffect() {
+    const element = document.getElementById("typewriter");
+    if (element) {
+      const text = element.innerHTML;
+      element.innerHTML = "";
+      let i = 0;
+      let speed = 60;
+      const type = () => {
+        if (i < text.length) {
+          element.innerHTML += text.charAt(i);
+          i++;
+          setTimeout(type, speed);
+        } else {
+          i = 0;
+          setTimeout(() => {
+            element.innerHTML = "";
+            type();
+          }, 1000);
+        }
+      }
+      type();
     }
   }
 
