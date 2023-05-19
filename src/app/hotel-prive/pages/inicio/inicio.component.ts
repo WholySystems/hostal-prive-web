642473e8e65912542e9edc314995ad4d3538de0b
Jduchman <jorge.duchman.27@unsch.edu.pe>
2023-05-19 00:03:08 -0500

    main completo

--- src/app/hotel-prive/pages/inicio/inicio.component.ts
+++ src/app/hotel-prive/pages/inicio/inicio.component.ts
@@ -14,17 +14,30 @@ export class InicioComponent implements OnInit{
   handleScroll(): void {
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     const element1 = document.getElementById("title") as HTMLDivElement;
+    const element2 = document.getElementById("numer1") as HTMLDivElement;
+    const element3 = document.getElementById("numer2") as HTMLDivElement;
+    const element4 = document.getElementById("numer3") as HTMLDivElement;
     if(scrollTop > 500){
-      this.mover(element1)
+      this.mover(element1);
       console.log(scrollTop)
-      element1.style.opacity = "1";
-      element1.style.transitionDuration = "1.5s";
+    } 
+    if (scrollTop > 1000){
+      this.mover(element2);
+    }
+    if (scrollTop > 1500){
+      this.mover(element3);
+    }
+    if (scrollTop > 2000){
+      this.mover(element4);
     }
   }
 
   mover( element : HTMLDivElement){
-    element.style.transform = 'translateY(0rem)'
+    element.style.transform = 'translateY(0rem)';
     element.style.transition = "all";
+    element.style.opacity = "1";
+    element.style.transitionDuration = "1.5s";
+    
   }
 
   ngOnInit(): void {
